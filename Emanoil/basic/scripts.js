2c17752956777a743c19cbefbd470ff671691782
Emanoil Karov <e_karov@yahoo.com>
2023-02-07 13:05:40 +0100

    Implement ex 2 - basic

--- Emanoil/basic/scripts.js
+++ Emanoil/basic/scripts.js
@@ -1,24 +1,50 @@
+// Select the submit button and attaching the "click"- eventlistener:
 document.getElementById("btn").addEventListener("click", displayNames);
 
+// The function that is called by raising the event:
 function displayNames() {
+    // Save the values from the inputs:
     const firstName = document.getElementById("fname").value;
     const lastName = document.getElementById("lname").value;
     const age = document.getElementById("age").value;
+    const profession = document.getElementById("profession").value;
 
-
+    // Setting a default color in case that a condition is not met (name - shorter than 5 letters):
     let color = "red";
     if (firstName.length > 5) {
         color = "green";
     }
-
-    const outputDiv = document.getElementById("output");
-
-    outputDiv.style.display = "block";
+    
+    // Displaying some message to user if we don't get input values:
     let outputHTML = "Please, enter some values.";
+    // In if we have the inputs, we replace the message with the HTML with the values:
     if (firstName && lastName) {
         outputHTML = `<span style="color: ${color}">${firstName}</span> ${lastName},  Age: ${age} years.`;
     }
-
-    document.getElementById("result").innerHTML = outputHTML;
+    
+    // Defining a background color for the output according to the profession:
+    let outputDivColor = "";
+    switch (profession) {
+        case("education"):
+        outputDivColor = "green";
+        break;
+        case("it"):
+        outputDivColor = "purple";
+        break;
+        case("science"):
+        outputDivColor = "blue";
+        break;
+        case("hospitality"):
+        outputDivColor = "yellow";
+        break;
+        default:
+            outputDivColor = "grey";
+        }
+        
+        //Making the output div visible with display: block - property and giving it the appropriate backgroung color:
+        document.getElementById("result").innerHTML = outputHTML;
+        const outputDiv = document.getElementById("output");
+        outputDiv.style.display = "block";
+        outputDiv.style.backgroundColor = outputDivColor;
 
 }
