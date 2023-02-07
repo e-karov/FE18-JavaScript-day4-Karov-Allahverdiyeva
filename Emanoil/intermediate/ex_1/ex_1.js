bd5b3c01044f47ab41254b3e881392ab02b1a778
Emanoil Karov <e_karov@yahoo.com>
2023-02-07 17:05:56 +0100

    Replace forEach loop with for..of loop by attaching the event listener

--- Emanoil/intermediate/ex_1/ex_1.js
+++ Emanoil/intermediate/ex_1/ex_1.js
@@ -1,11 +1,11 @@
 //Saving the HTML Collection returned from getElementsByTagName method and converting it to array:
-const images = Array.from(document.getElementsByTagName("img"));
+const images = document.getElementsByTagName("img");
+
 
 // Iterating over the array of img-elements and attach the EventListener to each element:
-images.forEach(element => {
-    element.addEventListener("click", hideSanta);
-});
-
+for (const img of images) {
+    img.addEventListener("click", hideSanta);
+}
 
 function hideSanta(e) {
 // Checking if the clicked element satisfies givven condition and if yes - adding to it a class "hidden" property:
