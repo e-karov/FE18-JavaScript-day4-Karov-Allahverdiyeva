9460638923721a20d9c9b37abfc94b2307460743
Emanoil Karov <e_karov@yahoo.com>
2023-02-07 14:44:44 +0100

    Add ex 1 - intermediate

---
+++ Emanoil/intermediate/ex_1/ex_1.js
@@ -0,0 +1,15 @@
+//Saving the HTML Collection returned from getElementsByTagName method and converting it to array:
+const images = Array.from(document.getElementsByTagName("img"));
+
+// Iterating over the array of img-elements and attach the EventListener to each element:
+images.forEach(element => {
+    element.addEventListener("click", hideSanta)
+});
+
+
+function hideSanta(e) {
+// Checking if the clicked element satisfies givven condition and if yes - adding to it a class "hidden" property:
+   if (e.target.getAttribute("alt") == "Santa Klaus") {
+    e.target.className = "hidden";
+   }
+}
